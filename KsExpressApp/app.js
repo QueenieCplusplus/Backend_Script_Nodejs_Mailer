6ccefdb532f4ef533b4efc549c4a2b9ef2364921
KatesAndroid <katesandroid@KatesAndroiddeMacBook-Pro.local>
2021-02-22 14:56:52 +0800

    add gmail mail sender feature (api token is required)

--- KsExpressApp/app.js
+++ KsExpressApp/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mailRouter = require('./routes/mailer');
 var nodemailRouter = require('./routes/nodemail');
+var gmailRouter = require('./routes/gmail');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mail', mailRouter);
 app.use('/nodemail', nodemailRouter);
+app.use('/gmail', gmailRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
